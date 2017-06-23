af6557401ccaeaab1df1cc955577df29790b42ce
guerric.delage <guerric.delage@telecom-paristech.fr>
2017-06-23 16:22:46 +0200

    Correcting bugs

--- preprocessing.py
+++ preprocessing.py
@@ -80,6 +80,107 @@ class Preprocessor:
         for action in return_dict['actions']:
             predicates.add(action)
         
+        # Store the variable predicates.
+        variable1 = []
+        pattern1="\s?(-?\w+\(X,\s?\w*)\s?\)"
+        words = re.findall(pattern1, text, flags=re.MULTILINE)
+        for word_text in words:
+                
+            word = list(re.findall("([\w-]+)", word_text))
+            variable1.append(word)
+        return_dict['variable_predicates'] = variable1
+        #est ce que ça prend en compte les prédicats (x, cste1, cste2)
+
+
+        variable2 = []
+        pattern2="\s?(-?\w+\(\s?\w*,\s?A)\s?\)"
+        words = re.findall(pattern2, text, flags=re.MULTILINE)
+        for word_text in words:
+                
+            word = list(re.findall("([\w-]+)", word_text))
+            variable2.append(word)
+        return_dict['variable_predicates'] += variable2
+        
+        
+        
+        variable3 = []
+        pattern3="\s?(-?\w+\(X,\s?A)\s?\)"
+        words = re.findall(pattern3, text, flags=re.MULTILINE)
+        for word_text in words:
+                    
+            word = list(re.findall("([\w-]+)", word_text))
+            variable3.append(word)
+        return_dict['variable_predicates'] += variable3
+        
+        
+        variableA = []
+        patternA="\s?(-?\w+\(X,\s?Y)\s?\)"
+        words = re.findall(patternA, text, flags=re.MULTILINE)
+        for word_text in words:
+                    
+            word = list(re.findall("([\w-]+)", word_text))
+            variableA.append(word)
+        return_dict['variable_predicates'] += variableA
+        
+        
+        variableB = []
+        patternB="\s?(-?\w+\(_,\s?_,\s?_)\s?\)"
+        words = re.findall(patternB, text, flags=re.MULTILINE)
+        for word_text in words:
+                    
+            word = list(re.findall("([\w-]+)", word_text))
+            variableB.append(word)
+        return_dict['variable_predicates'] += variableB
+        
+        
+        
+        
+        variableC = []
+        patternC="\s?(-?\w+\(X,\s?A)\s?\)"
+        words = re.findall(patternC, text, flags=re.MULTILINE)
+        for word_text in words:
+                    
+            word = list(re.findall("([\w-]+)", word_text))
+            variableC.append(word)
+        return_dict['variable_predicates'] += variableC
+        
+        
+        variableD = []
+        patternD="\s?(-?w+\(Y,\s?A)\s?\)"
+        words = re.findall(patternD, text, flags=re.MULTILINE)
+        for word_text in words:
+                    
+            word = list(re.findall("([\w-]+)", word_text))
+            variableD.append(word)
+        return_dict['variable_predicates'] += variableD
+        
+        
+        
+        variableE = []
+        patternE="\s?(-?\w+\(X,\s?A,\s?_)\s?\)"
+        words = re.findall(patternE, text, flags=re.MULTILINE)
+        for word_text in words:
+                    
+            word = list(re.findall("([\w-]+)", word_text))
+            variableE.append(word)
+        return_dict['variable_predicates'] += variableE
+        
+        
+        
+        variableF = []
+        patternF="\s?(-?\w+\(X)\s?\)"
+        words = re.findall(patternF, text, flags=re.MULTILINE)
+        for word_text in words:
+                
+            word = list(re.findall("([\w-]+)", word_text))
+            variableF.append(word)
+        return_dict['variable_predicates'] += variableF
+    
+    
+   
+        
+        
+    
         # Store the defaults.   
         pattern="default\(\s*?(-?\w*)\s*?\)"
         return_dict['defaults'] = re.findall(pattern, text, flags=re.MULTILINE)
@@ -110,7 +211,7 @@ class Preprocessor:
         predicates = {}
         logical_links = []
         for name in data_dict['predicates']:
-            
+            nb_arguments = 0
             if name[0]!='-':
                 negation_name = '-'+name
             else:
@@ -126,6 +227,9 @@ class Preprocessor:
     
             actionable = (name in data_dict['actions']) or (negation_name in data_dict['actions'])
             
+            for i in range(0, len(data_dict['variable_predicates'])):
+                if name == data_dict['variable_predicates'][i][0]:
+                    nb_arguments == len(data_dict['variable_predicates'][i])
 
     
             if name in data_dict['defaults']:
@@ -143,7 +247,7 @@ class Preprocessor:
     
             realised = name in data_dict['initial_situations']
                 
-            predicate = Predicate(name, value, actionable, realised, default)
+            predicate = Predicate(name, value, actionable, realised, default, nb_arguments)
             
             if negation_name in predicates :
                 negation_predicate = predicates[negation_name]
