9109aedd4bfe8a324f6889cf3e1d96fbd9e1b3b9
nrosset <nicolas.rosset@telecom-paristech.fr>
2017-06-28 10:38:52 +0200

    Add files via upload

--- proportionnelle.pl
+++ proportionnelle.pl
@@ -65,3 +65,30 @@
 	preference(confiance_des_electeurs_vis_a_vis_des_institutions,20).
 	preference(manque_de_confiance_des_electeurs_vis_a_vis_des_institutions,-10).
 	preference(justice,20).
+
+	% dictionnaire
+
+	dictionary(proportionnelle,la proportionnelle est en vigueur)
+	dictionary(-proportionnelle,la proportionnelle n est pas en vigueur)
+	dictionary(confiance_des_electeurs_vis_a_vis_des_institutions,les electeurs ont confiance en leurs institutions)
+	dictionary(-confiance_des_electeurs_vis_a_vis_des_institutions,les electeurs n ont pas confiance en leurs institutions)
+	dictionary(justice,le systeme est juste)
+	dictionary(-justice,le systeme n est pas juste)
+	dictionary(representativitee,le systeme est representatif)
+	dictionary(-representativitee,le systeme n est pas representatif)
+	dictionary(comptes_rendus_aux_responsables_politiques,les deputes rendent des comptes a leurs responsables politiques)
+	dictionary(-comptes_rendus_aux_responsables_politiques,les deputes ne rendent pas de comptes a leurs responsables politiques)
+	dictionary(perte_de_l_esprit_local,on perd l esprit local)
+	dictionary(-perte_de_l_esprit_local,on garde l esprit local)
+	dictionary(necessite_d_une_majorite,une majoritee est necessaire)
+	dictionary(-necessite_d_une_majorite,une majoritee n est pas necessaire)
+	dictionary(difficulte_a_gouverner,il est difficile de gouverner)
+	dictionary(-difficulte_a_gouverner,il est facile de gouverner)
+	dictionary(manque_de_confiance_des_electeurs_vis_a_vis_des_institutions,les electeurs manquent de confiance dans leurs institutions)
+	dictionary(-manque_de_confiance_des_electeurs_vis_a_vis_des_institutions,les electeurs ne manquent pas de confiance dans leurs institutions)
+	dictionary(passer(proportionnelle,-proportionnelle),on retire la proportionnelle)
+	dictionary(passer(-proportionnelle,proportionnelle),on instaure la proportionnelle)
+
+
+
+		
